f9c2b00e97ad57d75bd8a68c4489f24345ebe201
ericm <ericm@10.247.103.37>
2026-03-09 13:46:57 +0100

    atributos y constructoras de Jugador.java

--- Blackjack/src/juego/Jugador.java
+++ Blackjack/src/juego/Jugador.java
@@ -3,5 +3,14 @@ package juego;
 public class Jugador 
 {
 	private String nombre;
-	private int edad;
+	private int beiCoin;
+	private ListaCartas mano;
+	private int beiCoinJugados;
+	
+	
+	public Jugador(String pNomb) {
+		this.nombre = pNomb;
+		this.mano = new ListaCartas();
+		this.beiCoin = 300;
+	}
 }
