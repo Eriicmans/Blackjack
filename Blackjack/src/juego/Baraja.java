75feec55e518209fd3f1524bfe36de34b97806a6
ericm <ericm@DESKTOP-3DMQ9D8>
2026-03-24 10:30:21 +0100

    Baraja

--- Blackjack/src/juego/Baraja.java
+++ Blackjack/src/juego/Baraja.java
@@ -1,5 +1,29 @@
 package juego;
 
-public class Baraja {
+import java.util.ArrayList;
 
+public class Baraja {
+	
+	private static Baraja miBaraja;
+	private ArrayList<Carta> listaTodasCartas;
+	
+	private Baraja() {
+        this.listaTodasCartas = new ArrayList<Carta>();
+        this.inicializarBaraja(); // Se pone en la constructora porque nada mas crear la baraja se quieren tener todas las cartas
+    }
+	
+	public static Baraja getBaraja() {
+        if (miBaraja == null) {
+            miBaraja = new Baraja();
+        }
+        return miBaraja;
+    }
+	
+	
+	private void inicializarBaraja() {
+		
+		
+	}
 }
+
+
