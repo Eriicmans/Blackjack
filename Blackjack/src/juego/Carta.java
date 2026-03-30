2c8c02bf9eb394c44dd74184a7f1f43e5ddb6f4b
Eriicmans <128179962+Eriicmans@users.noreply.github.com>
2026-03-30 12:07:11 +0200

    Change fields from protected to private in Carta class

--- Blackjack/src/juego/Carta.java
+++ Blackjack/src/juego/Carta.java
@@ -1,8 +1,8 @@
 package juego;
 
 public abstract class Carta {
-    protected String palo;
-    protected String valorString; //se usa string para imprimir que es una J,Q,K
+    private String palo;
+    private String valorString; //se usa string para imprimir que es una J,Q,K
 
     public Carta(String pPalo, String pValorString) {
         this.palo = pPalo;
